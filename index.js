e5a8320c53f7e61dabe674cfd75d433502d549a9
Lorraineken <lorrainekupa@gmail.com>
2022-11-22 23:10:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+//const main = document.getElementsByTagName("main");
+//const secondChild = main.querySelector("main"); 
+main.remove()
+
+const newHeader = document.createElement("h1");
+//const currentHeader=document.getElementById("victory")
+//document.body.insertBefore(newHeader,currentHeader)
+newHeader.id='victory';
+newHeader.textContent = "LORRAINE is the champion";
+document.body.append(newHeader);
+
+
+
+//const h1 = document.createElement("h1");
+
+//newHeader.append(h1);
